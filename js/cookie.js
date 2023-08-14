fe1479eae3c07311091835913c58d006d81af22d
felipedelosh <doctorfhernandez@hotmail.com>
2023-08-14 05:04:12 -0500

    Hello Cookie

--- js/cookie.js
+++ js/cookie.js
@@ -4,3 +4,8 @@
 
 localStorage.setItem('autor', 'felipdelosh');
 localStorage.setItem('name', 'unknow');
+
+if(localStorage.getItem('date_first_time_visited_page') === null){
+    const fecha = new Date();
+    localStorage.setItem('date_first_time_visited_page', fecha.toDateString());
+}
