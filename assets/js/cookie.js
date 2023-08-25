9222544186e8eab1ae9628036e5cdb01f739b3d4
felipedelosh <doctorfhernandez@hotmail.com>
2023-08-25 11:56:01 -0500

    Token

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -8,3 +8,9 @@ localStorage.setItem('name', 'portfolio - felipedelosh');
 if(localStorage.getItem('language') === null){
     localStorage.setItem('language', 'en');
 }
+
+
+if(localStorage.getItem('token') === null){
+    localStorage.setItem('token', 'kmzwa8awaa');
+}
+
