4773d086e874e3eab08beed70122e0113bc09a38
felipedelosh <doctorfhernandez@hotmail.com>
2023-08-25 11:53:16 -0500

    data cookie

--- assets/js/cookie.js
+++ assets/js/cookie.js
@@ -2,7 +2,8 @@
  * Save Files in HardDrive
 */
 
-localStorage.setItem('autor', 'felipedelosh');
+localStorage.setItem('author', 'felipedelosh');
+localStorage.setItem('name', 'portfolio - felipedelosh');
 
 if(localStorage.getItem('language') === null){
     localStorage.setItem('language', 'en');
